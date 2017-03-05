fb5974e55225aa4890faa10ce03cff881abc2988
witzawitz <witzawitz@gmail.com>
2017-03-05 21:39:39 +0300

    Request section.

--- js/bie.js
+++ js/bie.js
@@ -5,4 +5,8 @@ $(document).ready(function () {
 	}).mouseleave(function () {
 		$('.schema-popup').removeClass('active');
 	});
+	$('.action-examples .request .nav, .action-examples .response .nav').each(function () {
+		if ($(this).find('li.active').length == 0)
+			$(this).find('a:first').tab('show');
+	});
 });
